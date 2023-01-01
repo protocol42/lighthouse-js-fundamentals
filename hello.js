d6789082a243d8e79b956b10bf5c4f5b52362187
protocol42 <jhull@protocolrescue.com>
2022-12-31 20:15:26 -0700

    functs returning/logging value added to hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,44 @@
-const sayHello = function (name) {
-  console.log('hello ' + name);
+{
+  const sayHello = function (name) {
+    console.log('hello ' + name);
 
+  }
+
+  sayHello('Caliban');
+  sayHello('Miranda');
+  sayHello('Ferdinand');
 }
 
-sayHello('Caliban');
-sayHello('Miranda');
-sayHello('Ferdinand');
+{
+  const sayHelloToConsole = function (name) {
+    console.log(name);
+  }
+
+  sayHelloToConsole('john');
+}
+
+{
+  const sayHelloToConsole = function (name) {
+    return name;
+  }
+
+  const greeting = sayHelloToConsole('john1');
+
+  console.log(greeting);
+}
+
+
+{
+  const returnSayHello = function (name) {
+    return 'Hello ' + name;
+  }
+
+  const greeting = returnSayHello('John');
+  console.log(greeting);
+}
+
+
+
+
+
+
