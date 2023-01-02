5df26b35d7a404dae7aab779279bada6ea1c05f6
protocol42 <jhull@protocolrescue.com>
2023-01-02 14:53:46 -0700

    formatting perfect-shot.js

--- perfect-shot.js
+++ perfect-shot.js
@@ -1,30 +1,30 @@
-const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north']
+const moves = ['north', 'north', 'west', 'west', 'north', 'east', 'north'];
 
 const finalPosition = function (moves) {
   // Your code in here ...
-  let paradeRoute = [0, 0]
+  let paradeRoute = [0, 0];
 
   for (move of moves) {
     if (move === 'north') {
       // console.log('north')
-      paradeRoute[1] = paradeRoute[1] += 1
+      paradeRoute[1] = paradeRoute[1] += 1;
 
     } else if (move === 'south') {
       // console.log('south')
-      paradeRoute[1] = paradeRoute[1] -= 1
+      paradeRoute[1] = paradeRoute[1] -= 1;
 
     } else if (move === 'east') {
       // console.log('east')
-      paradeRoute[0] = paradeRoute[0] += 1
+      paradeRoute[0] = paradeRoute[0] += 1;
 
     } else if (move === 'west') {
       // console.log('west')
-      paradeRoute[0] = paradeRoute[0] -= 1
+      paradeRoute[0] = paradeRoute[0] -= 1;
 
     }
   }
-  return paradeRoute
-}
+  return paradeRoute;
+};
 
 finalPosition(moves);
 // conditionals structure - done
