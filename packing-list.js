4815fe74e283a667745b5b619fd17fc703955d81
protocol42 <jhull@protocolrescue.com>
2023-01-03 19:30:42 -0700

    update file - packing-list.js - tinker file for "iterating over arrays" - update formatting, add funct, while loop

--- packing-list.js
+++ packing-list.js
@@ -1,9 +1,20 @@
+// array iteration
+
 const packingList = ['bowls', 'plates', 'pots', 'pans', 'eating utensils', 'glasses', 'cups', 'cooking utensils'];
 
 
+// for loop
 console.log('kitchen stuff to pack:');
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+// while loop
+console.log('kitchen stuff to pack:');
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
 
 
