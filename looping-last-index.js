abf96f9aadf99b7344b4e3baba23b28b7876c238
protocol42 <88254215+protocol42@users.noreply.github.com>
2023-01-06 15:06:50 -0700

    new file - looping-last-index.js - answers for assignment "looping last-index-of"

---
+++ looping-last-index.js
@@ -0,0 +1,14 @@
+
+const lastIndexOf = function (array, num) {
+  const findIndex = (element) => element === num
+  return array.findLastIndex(findIndex);
+}
+
+
+
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
